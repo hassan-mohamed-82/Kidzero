d2b9950fa98fc276144c3e6c2ddada965bbb72b0
miskovy <mazenkhairy200@gmail.com>
2026-01-14 11:17:25 +0200

    Add parent plans route for parents
    Introduced a new /parentplans endpoint in the users router, handled by a dedicated router that exposes a GET endpoint to retrieve all parent plans. This enables parents to access available plans via the API.

--- src/routes/users/parent/parentPlans.ts
+++ src/routes/users/parent/parentPlans.ts
@@ -1,2 +1,9 @@
 import { Router } from "express";
 import { getAllParentPlans } from "../../../controllers/superadmin/parentPlan";
+
+
+const router = Router();
+
+router.get("/", getAllParentPlans);
+
+export default router;
