bf778aa86adae791bbc076acd33664789109ff0e
miskovy <mazenkhairy200@gmail.com>
2026-02-08 14:39:27 +0200

    Add GET route for parent org-service payment
    Expose GET /org-service/:id in parentPayment router to fetch a parent organization-service payment by ID. Imports getparentPaymentOrgServicebyId from the parent payment controller and wires it into the router; includes minor formatting adjustments.

--- src/routes/users/parent/parentPayment.ts
+++ src/routes/users/parent/parentPayment.ts
@@ -6,7 +6,8 @@ import {
     getparentInstallmentById,
     createParentPayment,
     createParentPaymentOrgService,
-    payServiceInstallment
+    payServiceInstallment,
+    getparentPaymentOrgServicebyId
 } from "../../../controllers/users/parent/payment";
 import { catchAsync } from "../../../utils/catchAsync";
 const router = Router();
@@ -16,7 +17,9 @@ router.get("/", catchAsync(getParentPayments));
 router.get("/:id", catchAsync(getParentPaymentbyId));
 router.post("/", catchAsync(createParentPayment));
 router.post("/org-service", catchAsync(createParentPaymentOrgService));
+router.get("/org-service/:id", catchAsync(getparentPaymentOrgServicebyId));
 router.post("/pay-installment", catchAsync(payServiceInstallment));
 router.get("/installments", catchAsync(getparentInstallments));
 router.get("/installments/:id", catchAsync(getparentInstallmentById));
+
 export default router;
